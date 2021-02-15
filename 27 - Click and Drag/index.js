d6522bcee9e73ab796fffe02f7a49f4c6567c5bb
yhancsx <johnbae57@naver.com>
2021-02-15 23:44:07 +0900

    27 click and drag

---
+++ 27 - Click and Drag/index.js
@@ -0,0 +1,28 @@
+const slider = document.querySelector(".items");
+
+let isDown = false;
+let startx;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  slider.classList.remove("active");
+  isDown = false;
+});
+slider.addEventListener("mouseup", () => {
+  slider.classList.remove("active");
+  isDown = false;
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
